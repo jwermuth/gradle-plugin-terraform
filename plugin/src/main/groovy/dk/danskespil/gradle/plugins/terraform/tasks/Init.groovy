cc1dded5933a3febc25390ae2bd3cd087f60c14d
Zhelyazkov, Yanko <yanko.zhelyazkov@hybris.com>
2018-05-23 22:17:03 -0400

    Allow for backend configuration support

--- plugin/src/main/groovy/dk/danskespil/gradle/plugins/terraform/tasks/Init.groovy
+++ plugin/src/main/groovy/dk/danskespil/gradle/plugins/terraform/tasks/Init.groovy
@@ -1,7 +1,11 @@
 package dk.danskespil.gradle.plugins.terraform.tasks
 
 import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFiles
+
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecSpec
 
@@ -9,14 +13,32 @@ class Init extends TerraformBaseTask {
     @OutputFiles
     FileCollection outputFilesSoGradleOnlyBuildsWhenItChanges = project.fileTree('.terraform/terraform.tfstate')
 
+    @Optional
+    @Input
+    Map<String, String> backendConfig = [:]
+
+    @Internal
+    List extraArgs = []
+
     @TaskAction
     action() {
         commandLine.addToEnd('terraform', 'init')
 
+        if (!backendConfig.isEmpty()) {
+          extraArgs.add('-backend=true')
+        }
+
+        backendConfig.each{ k, v -> extraArgs.add('-backend-config=' + k + '=' + v) }
+
         executor.executeExecSpec(this, { ExecSpec e ->
             e.commandLine this.commandLine
+            e.args extraArgs
             e.workingDir project.projectDir
         })
+    }
+
+    def backendConfig(String key, String value) {
+        backendConfig.put(key, value)
     }
 
     @Override
